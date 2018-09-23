8683db3b88168c8550313975ea5f177c49e51058
Ben Camilleri <camilleriben@gmail.com>
2018-09-23 19:08:47 +0200

    New nodes will take new port and alert all other nodes

--- blockchain.py
+++ blockchain.py
@@ -4,8 +4,10 @@ import websockets
 import hashlib
 import json
 #https://websockets.readthedocs.io/en/stable/intro.html
-nodes = dict()
+
+
 serverDomain = None
+nodes = dict()
 
 if len(sys.argv) > 1 :
     serverDomain = sys.argv[1]
@@ -17,9 +19,9 @@ this_node_port = 1
 
 print("Starting....")
 
-print("First trying to connect to ws host '{serverDomain} on port 1 to get all nodes")
+print(f"First trying to connect to ws host '{serverDomain} on port 1 to get all nodes")
 
-async def client_all_nodes():
+async def get_all_nodes():
     print(f"Connecting to ws://{serverDomain}:1...")
     try:
         async with websockets.connect(f"ws://{serverDomain}:1") as websocket:
@@ -27,54 +29,89 @@ async def client_all_nodes():
             data_received = await websocket.recv()
             print(f"data_received={data_received}")
             global nodes
-            nodes = json.loads(data_received)
+            nodes_rec = json.loads(data_received)
+            for node in nodes_rec:
+                nodes[node] = None
             global this_node_port
             this_node_port = len(nodes) + 1
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+    except:        
         print(f"Failed to connect to ws://{serverDomain}:1...")
+        print("Unexpected error:", sys.exc_info()[0])
     finally:
         print("Continueing to host socket")
     
-asyncio.get_event_loop().run_until_complete(client_all_nodes())
+asyncio.get_event_loop().run_until_complete(get_all_nodes())
+
+#-------------------------------------------------------------------------
+
+outbox = asyncio.Queue()
+broadcast_outbox = asyncio.Queue()
+
+async def socket_handler(websocket, path):
+    consumer_task = asyncio.ensure_future(consumer_handler(websocket, path))
+    producer_task = asyncio.ensure_future(producer_handler(websocket, path))
+    done, pending = await asyncio.wait([consumer_task, producer_task],
+        return_when=asyncio.FIRST_COMPLETED,)
+    for task in pending:
+        task.cancel()
 
 
+async def consumer_handler(websocket, path):
+    async for message in websocket:
+        await consumer(message, websocket)
 
-async def node_socket(websocket, path):
-    # Register node
-    print(f"WebSocket connection: {websocket}")
-    #await register(websocket)
-    try:
-        #await websocket.send(state_event())
-        async for message in websocket:
-            data = json.loads(message)
-            print(f'data[msgtype]={data["msgtype"]}   data[msgpayload]={data["msgpayload"]}')
-            if data["msgtype"] == "all_nodes":
-                return_data = json.dumps(nodes)
-                print(return_data)
-                await websocket.send(return_data)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-    finally:
-        #await unregister(websocket)
-        pass
+async def producer_handler(websocket, path):
+    while True:
+        message = await outbox.get()
+        await websocket.send(message)
 
+async def consumer(message, websocket):
+    print("consumer message:", message)
 
-    #nodes[this_node_port] = websocket
-    #try:
-    #    print(f"websocket: {websocket} path:{path}")
-    #finally:
-    #    # Unregister node
-    #    del nodes[this_node_port]
+    data = json.loads(message)
+    print(f'data[msgtype]={data["msgtype"]}   data[msgpayload]={data["msgpayload"]}')
+    if data["msgtype"] == "all_nodes":
+        return_data = json.dumps(list(nodes.keys()))
+        print(return_data)
+        await outbox.put(return_data)
+    elif data["msgtype"] == "new_node":
+        nodes[data["msgpayload"]] = websocket
 
+async def broadcaster_handler():
+    while True:
+        message = await broadcast_outbox.get()
+        for node in nodes:
+            try:
+                if node != this_node_port:
+                    if nodes[node] == None or nodes[node].closed == True:
+                        async with websockets.connect(f"ws://{serverDomain}:{node}") as websocket:
+                            await websocket.send(message)
+                            nodes[node] = websocket
+                    else:
+                        await nodes[node].send(message)
+            except:
+                print(f"Failed to alert node: {node} message:{message}")
+                print("Unexpected error:", sys.exc_info()[0])
 
-print(f"serving on port {len(nodes)+1}")
-start_server = websockets.serve(node_socket, serverDomain, this_node_port)
+asyncio.ensure_future(broadcaster_handler())
+
+async def alert_all_nodes(type, data):
+    print(f"alerting all nodes")
+    json_obj = {}
+    json_obj['msgtype'] = type
+    json_obj['msgpayload'] = data
+    json_data = json.dumps(json_obj)
+    await broadcast_outbox.put(json_data)
 
 nodes[this_node_port] = None
+asyncio.get_event_loop().run_until_complete(alert_all_nodes("new_node", this_node_port))
+asyncio.get_event_loop().run_until_complete(alert_all_nodes("new_node", this_node_port))
 
+print(f"Serving on port {this_node_port}")
+start_server = websockets.serve(socket_handler, serverDomain, this_node_port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
-#hash = hashlib.sha256(b"hello world").hexdigest()
-#print (hash)
+
+  
+     
