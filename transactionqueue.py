53f10b155443460788b5bfa5ee251f4838275f3e
Ben Camilleri <camilleriben@gmail.com>
2018-11-25 13:52:43 +0100

    Use of Quart instead of Flask for web framework

--- transactionqueue.py
+++ transactionqueue.py
@@ -1,25 +1,29 @@
+import sys
+from quart import Quart, request, render_template
 from blockchain import Transaction
-from flask import Flask, request, render_template
 import json
 import node 
-import asyncio
 import random
+import asyncio
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 @app.route('/')
 def index():
     return "OK"
 
 @app.route('/tx', methods=['POST'])
-def transaction():
-  if request.method == 'POST':
-    new_tx = request.get_json()
+async def transaction():
+    new_tx = await request.get_json()
     print(f"New transaction FROM: {new_tx['from']} TO: {new_tx['to']} AMOUNT: {new_tx['amount']}\n")
     #TODO:VALIDATION HERE
     transaction = Transaction(str(new_tx['from']), str(new_tx['to']), str(new_tx['amount']))
-    asyncio.run(node.AddTransaction(node.broadcast_outbox, transaction))
+    try:
+        await node.AddTransaction(node.broadcast_outbox, transaction)
+    except:
+        print("Unexpected error:", sys.exc_info()[0], sys.exc_info()[1])
     return "OK"
 
 if __name__ == "__main__":
     app.run(port=random.randint(5000,6000))
+    #app.run(port=5000)
